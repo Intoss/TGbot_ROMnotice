471571a2963e2610f61ac9d10b1f1e45ea54ac64
Intoss <125802662+Intoss@users.noreply.github.com>
2025-09-04 09:32:29 +0300

    Update tg_token

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from telegram.ext import (ApplicationBuilder, ContextTypes, CommandHandler,
 # ---------------- CONFIG ----------------
 OWNER_ID = 1850766719  # твой ID - владелец бота
 #TOKEN = os.environ.get("TELEGRAM_TOKEN")  # поставь токен в ENV на Replit
-TOKEN = os.environ["BOT_TOKEN"]
+TOKEN = os.environ["TELEGRAM_TOKEN"]
 DB_PATH = "bot.db"
 
 # Two clans
@@ -699,4 +699,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
